0521b7dbbc860a9d216ff77bb7b09c61c4b358f1
xiaofandegeng <434962239@qq.com>
2020-06-18 21:33:01 +0800

    整合servlet

---
+++ springbootdemo02/src/main/java/com/ning4256/springboot/Springbootdemo02Application.java
@@ -0,0 +1,16 @@
+package com.ning4256.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+//扫描@WebServlet端口
+@ServletComponentScan
+public class Springbootdemo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo02Application.class, args);
+    }
+
+}
---
+++ springbootdemo02/src/test/java/com/ning4256/springboot/Springbootdemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ning4256.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
